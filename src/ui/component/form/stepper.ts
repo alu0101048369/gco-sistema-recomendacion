193a0d5cd1d5375d2218123c49cdd5a1c959fcec
Miguel-Dorta <contact@migueldorta.com>
2023-10-16 11:29:46 +0100

    Change body in form stepper event

--- src/ui/component/form/stepper.ts
+++ src/ui/component/form/stepper.ts
@@ -1,7 +1,10 @@
-const steps = document.getElementById("stepper")!.children;
-let currentStep = 1;
+const stepBodies = document.getElementById("form-body")!.children;
+const stepIndexes = document.getElementById("stepper")!.children;
+let currentStep = 0;
 
 export function nextStep() {
-    steps[currentStep].classList.replace("bg-secondary", "bg-primary");
+    stepBodies[currentStep].classList.add("d-none");
     currentStep++;
+    stepBodies[currentStep].classList.remove("d-none");
+    stepIndexes[currentStep].classList.replace("bg-secondary", "bg-primary");
 }
