a9a14750a2ae3085d7228b146b577fda629adc39
Laura Dorta <alu0101400624@ull.edu.es>
2023-10-20 11:45:34 +0100

    Cosine Dist Tested

--- src/js/metric/cosine_dist.ts
+++ src/js/metric/cosine_dist.ts
@@ -19,31 +19,30 @@ export function cosineDistance(
   if (n === 0) {
     return undefined;
   }
-
   // Multiplication of x and y
   let multp: number = 0;
   for (let i = 0; i < filteredX.length; i++) {
     multp += (filteredX[i]* filteredY[i]);
   }
-  
   //Square root of x
   let sqrt_x : number = 0;
   for (let i = 0; i < filteredX.length; i++) {
     const element = filteredX[i];
     sqrt_x += Math.pow(element,2);
-    sqrt_x = Math.sqrt(sqrt_x);
   }
+  sqrt_x = Math.sqrt(sqrt_x);
 
   //Square root of y
   let sqrt_y : number = 0;
   for (let i = 0; i < filteredY.length; i++) {
     const element = filteredY[i];
     sqrt_y += Math.pow(element,2);
-    sqrt_y = Math.sqrt(sqrt_y);
   }
+  sqrt_y = Math.sqrt(sqrt_y);
 
   //Multiplication of square roots
   let sqrt_xy : number = sqrt_x*sqrt_y;
+
 
   // Check division by 0
   if (multp === 0 || sqrt_xy === 0) {
@@ -57,7 +56,7 @@ export function cosineDistance(
 
 // TEST
 
-const vectorX: (number | undefined)[] = [1, 2, 3, 4, undefined, 5];
+/* const vectorX: (number | undefined)[] = [1, 2, 3, 4, undefined, 5];
 const vectorY: (number | undefined)[] = [2, 3, undefined, 4, 5, 6];
 
 let result = cosineDistance(vectorX, vectorY);
@@ -66,4 +65,4 @@ if (result !== undefined) {
   console.log(`La distancia del coseno es: ${result}`);
 } else {
   console.log("No hay suficientes elementos para calcular la distancia.");
-}
+} */
