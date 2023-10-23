56b80e8b99c3adfa664d18d627ca90b6fa66c8ef
Miguel-Dorta <contact@migueldorta.com>
2023-10-23 12:13:12 +0100

    Almost done

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { recomendation } from "./logic/function";
 import { parseFile } from "./logic/input/parse/file";
 import { newForm } from "./ui/component/form";
 import { switchView } from "./ui/component/main";
+import { buildTable } from "./ui/component/table";
 
 async function main() {
-  const vars = await newForm();
+  /*const vars = await newForm();
 
   let scores;
   try {
@@ -25,7 +26,32 @@ async function main() {
         scores: scores.normValues,
     }));
 
-    switchView();
+    switchView();*/
+
+    const result = recomendation({
+      metric: "pearson",
+      neighbours: 2,
+      prediction: "simple",
+      scores: [
+        [1.00, 0.50, 0.75, 0.75, undefined],
+        [0.50, 0.00, 0.25, 0.50, 0.50],
+        [0.75, undefined, 0.75, 0.50, 1.00],
+        [0.50, 0.50, 0.00, 1.00, 0.75],
+        [0.00, 1.00, 1.00, 0.25, 0.00],
+      ],
+    });
+    console.log(result);
+    buildTable({
+      max: 5,
+      min: 1,
+      normValues: [
+        [1.00, 0.50, 0.75, 0.75, undefined],
+        [0.50, 0.00, 0.25, 0.50, 0.50],
+        [0.75, undefined, 0.75, 0.50, 1.00],
+        [0.50, 0.50, 0.00, 1.00, 0.75],
+        [0.00, 1.00, 1.00, 0.25, 0.00],
+      ]
+    }, result);
 }
 
 main();
--- src/ui/component/table/index.ts
+++ src/ui/component/table/index.ts
@@ -1,1 +1,64 @@
-const table = document.getElementById("tableDataWithPredictions");
+import { recomendation_output_result } from "../../../logic/common/out_types/recomendation_output_result";
+import { Scores } from "../../../logic/input/parse/file";
+
+const table = document.getElementById("tableDataWithPredictions")!;
+const txtOp = document.getElementById("txtOp") as HTMLParagraphElement;
+const tableCorrelations = document.getElementById("tableCorrelations")!;
+
+export function buildTable(originalScores: Scores, out: recomendation_output_result) {
+    (document.getElementById("tableDataHeader") as HTMLTableCellElement).colSpan = originalScores.normValues[0].length;
+    let predictionCounter = 0;
+
+    originalScores.normValues.forEach((row, nrow) => {
+        const tr = document.createElement("tr");
+        const th = document.createElement("th");
+        th.innerText = String(nrow+1);
+        tr.appendChild(th);
+
+        row.forEach((col, ncol) => {
+            const td = document.createElement("td");
+            if (col === undefined) {
+                col = normalizeVal(out.result_matrix[nrow][ncol] as number, originalScores.max, originalScores.min);
+
+                const a = document.createElement("a");
+                a.href = "#";
+                a.innerText = String(col);
+                const predictionIndex = predictionCounter; // Save as constant to preserve between iterations
+                a.addEventListener("click", evt => {
+                    evt.preventDefault();
+                    txtOp.innerText = out.elements_logs[predictionIndex].operation_logs;
+
+                    tableCorrelations.innerHTML = "";
+                    out.elements_logs[predictionIndex].correlation.all_neighbours.
+                        sort((a, b) => a.index - b.index).
+                        forEach(correl => {
+                            const tr = document.createElement("tr");
+
+                            const th = document.createElement("th");
+                            th.innerText = String(correl.index+1);
+                            tr.appendChild(th);
+                            
+                            const td = document.createElement("td");
+                            td.innerText = String(correl.correlation);
+                            tr.appendChild(td);
+
+                            tableCorrelations.appendChild(tr);
+                        });
+                });
+
+                predictionCounter++;
+                td.appendChild(a);
+            } else {
+                col = normalizeVal(col, originalScores.max, originalScores.min);
+                td.innerText = String(col);
+            }
+            tr.appendChild(td);
+        });
+
+        table.appendChild(tr);
+    });
+}
+
+function normalizeVal(val: number, max: number, min: number): number {
+    return (val*(max-min))+min;
+}
